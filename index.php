eb563df97df8d9b1379990bb8e1846f2381cf412
Geg <spbromanov@mail.ru>
2019-03-16 22:12:10 +0300

    превый коммит

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    
+    
+    <?php
+    
+    
+    $a = 5;
+    $b = 5.6;
+    $name = 'Geg';
+    $bul = true;
+    
+    echo "Переменная a = $a ";
+    echo "Переменная b = $b";
+    echo "Переменная name = $name";
+    echo "Переменная bul = $bul";
+    
+    
+    ?>
+    
+    
+</body>
+</html>
