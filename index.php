206b104373e31b86877837040c583c714e80cb02
vadim romanov <spbromanov@mail.ru>
2019-04-16 20:40:00 +0300

    third commit

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 adsad
 
 Второе изменение
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur dignissimos dolores earum iusto rem. Assumenda consequuntur ipsa maxime nemo provident rerum vero voluptatem! Eum ipsum iste odio placeat, ratione totam!
 
 </body>
 </html>
